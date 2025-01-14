830b4142e41e06857254b12deccc70d674ae90b9
JackKirbyCook82 <jackcookchee@yahoo.com>
2025-01-13 21:25:11 -0600

    Updated 1/10/2024

---
+++ interactive.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 13 2025
+@name:   Interactive Market Objects
+@author: Jack Kirby Cook
+
+"""
+
+__version__ = "1.0.0"
+__author__ = "Jack Kirby Cook"
+__all__ = []
+__copyright__ = "Copyright 2025, Jack Kirby Cook"
+__license__ = "MIT License"
+
+
+
+
+
